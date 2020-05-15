f7b8b7786833d64056434e03ae36d5ac6ab7be16
Mark McDonald <markwilliammcdonald@gmail.com>
2020-05-15 13:58:34 -0500

    added in the basic html framework and the questions to the quiz w/ correct answers all within the array variable called questions

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+
+// Quiz questions. Trying an array for everything, and making the answers and questions strings of that array
+let questions = [
+    {
+        question:"Question 1:  What is the corect top-line of an HTML file?",
+        answerA: "<!DOCUMENTTYPE html>",
+        answerB: "<!DOCTYPE html>",
+        answerC: "<!DOCTYPE CSS>",
+        answerD: "<DOCtype html>",
+        correct: "B"
+
+    },{
+        question:"Question 2:  What does CSS stand for?",
+        answerA: "Cascading Style Sheets",
+        answerB: "Customized Style Sheets",
+        answerC: "Central Stagnant Section",
+        answerD: "Clear Symbol System",
+        correct: "A"
+    },{
+        question:"Question 3:  What is the correct syntax for declaring an empty array 'a' in Javascript?",
+        answerA: "a = [];",
+        answerB: "let array = ();",
+        answerC: "let a = [];",
+        answerD: "Array = [empty];",
+        correct: "C"
+    },{
+        question:"Question 4:  Inside which HTML element do we put the JavaScript?",
+        answerA: "<script>",
+        answerB: "<js>",
+        answerC: "<scripting>",
+        answerD: "<javascript>",
+        correct: "A"
+    },{
+        question:"Question 5:  How do you create a function in JavaScript called myFunction?",
+        answerA: "function:myFunction",
+        answerB: "function = myFunction()",
+        answerC: "function myFunction()",
+        answerD: "myFunction = function",
+        correct: "C"
+    }
+];
+
+//Need a check answer function 
