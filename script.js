a2af7b759361334cf4103f27965862156840d92b
Mark McDonald <markwilliammcdonald@gmail.com>
2020-05-16 04:45:05 -0500

    added some logic and pathways to render the first question after pressing start. Don't have verification of whether the answer was correct or not, but question is popping up in the correct spot and the answers are being filled in the correct answer spots from the javascript

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 //Select the elements
-let answerA = document.querySelector("#answerA");
-let answerB = document.querySelector("#answerB");
-let answerC = document.querySelector("#answerC");
-let answerD = document.querySelector("#answerD");
-let startButton = document.getElementById("start");
-let beginDiv = document.querySelector("#instructions");
-let questionDiv = document.querySelector("#question");
+var navTimer = document.querySelector("#time");
+var answerA = document.querySelector("#answerA");
+var answerB = document.querySelector("#answerB");
+var answerC = document.querySelector("#answerC");
+var answerD = document.querySelector("#answerD");
+var startButton = document.getElementById("startBtn");
+var beginDiv = document.querySelector("#instructions");
+var questionDiv = document.querySelector("#question");
+var quizCard = document.querySelector("#quizCard");
 
 
 //Declare variables
 
 //Will use this to track the questions left in the quiz
-let i = 0;
+var i = 0;
 
 //Track the seconds remaining 
-let secondsRemain = 90;
+var secondsRemain = 90;
 
 //Used to store the seconds remaining in the quiz
-let interval = 0;
+var interval = 0;
+
+//track the card the user is on
+var activeDiv = beginDiv; 
 
 // Quiz questions and answers. Trying an array for everything.
-let questions = [
+var questions = [
     [
         "Question 1:  What is the corect top-line of an HTML file?",
         "A. <!DOCUMENTTYPE html>",
@@ -60,17 +65,64 @@ let questions = [
     ]
 ];
 
-//Need a function to start the quiz
-function beginQuiz(){
-
-}
-
-//Need a function to load the question
-function showQuestion() {
-
+//Need a function to show the questions
+function showQuestion(index) {
+    questionDiv.innerHTML = questions [index][0];
+    answerA.value = questions[index][1];
+    answerB.value = questions[index][2];
+    answerC.value = questions[index][3];
+    answerD.value = questions[index][4];
 }
 
 //Need a check answer function 
-function checkAnswers () {
-    
+function checkAnswers (index) {
+    var select = "";
+    if (index === 1) {
+        select = answerA;
+    } else if (index === 2) {
+        select = answerB;
+    } else if (index === 3) {
+        select = answerC;  
+    } else {
+        select = answerD;
+    }
+    ///have to do more here for the -10 seconds wrong answers
 }
+
+//Need a function to start the quiz and start the timer countdown in the nav bar
+function beginQuiz(){
+    interval = setInterval(function() {
+        navTimer.innerHTML = secondsRemain;
+        secondsRemain = secondsRemain -1;
+        navTimer.innerHTML = secondsRemain;
+
+        if (secondsRemain === 0) {
+            navTimer.innerHTML = secondsRemain;
+            clearInterval(interval);
+        }
+    }, 1000);
+
+    beginDiv.classList.toggle("collapse");
+    quizCard.classList.toggle("collapse");
+    activeDiv = quizCard;
+    showQuestion(i);
+}
+
+
+
+//Need an eventListener for the Start button click
+startButton.addEventListener("click", beginQuiz);
+
+//Need tracking of addEventListner for ever answer selected 
+answerA.addEventListener("click", function () {
+    checkAnswers(1);
+});
+answerB.addEventListener("click", function () {
+    checkAnswers(2);
+});
+answerC.addEventListener("click", function () {
+    checkAnswers(3);
+});
+answerD.addEventListener("click", function () {
+    checkAnswers(4);
+});
